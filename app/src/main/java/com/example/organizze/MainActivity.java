bbf866121d3c5c751f2c098d6c857e51a6283839
gustavoamodei <gustavosouza407@gmail.com>
2023-06-23 01:20:15 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.organizze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.organizze.Activity.CadastroActivity;
+import com.example.organizze.Activity.LoginActivity;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorSlide)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorSlide)
+                .fragment(R.layout.intro_2)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorSlide)
+                .fragment(R.layout.intro_3)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorSlide)
+                .fragment(R.layout.intro_4)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorSlide)
+                .fragment(R.layout.intro_cadastro)
+                .build());
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent( this, LoginActivity.class));
+    }
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+}
